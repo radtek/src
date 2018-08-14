e204a36761a46ecbe71d0298a551869b4fe65b73
Xiaolong Zhao <zxl881203@126.com>
2018-08-14 22:19:00 +0800

    0814
    new list,new edit ,updated

---
+++ PM/WeChat/log/newlog/newlist.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WeChat_log_newlist : System.Web.UI.Page
+{
+
+public string userID = string.Empty;
+    public string mlname = string.Empty;
+    public string keyID = string.Empty;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
